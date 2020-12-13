2756cccf4309f56f3a6b69e67956aedd2380f5f9
Valentin Vago <zeropaper@irata.ch>
2020-12-13 14:53:50 +0100

    follow mouse movement

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,50 @@
-import * as React from "react";
-import "./styles.css";
+import * as React from 'react';
+import './styles.css';
 
 import {
   DoomFace,
-  // levelNumber,
-  // variantLetter,
-} from "./components/DoomFace/DoomFace";
+  MouseFollowingDoomFace,
+  LevelNumber,
+} from './components/DoomFace/DoomFace';
 
 export default function App() {
   const faces = [
-    "abcdefgh",
-    "abcdefgh",
-    "abcdefgh",
-    "abcdefgh",
-    "abcdefgh",
-    "ab"
+    'abcdefgh',
+    'abcdefgh',
+    'abcdefgh',
+    'abcdefgh',
+    'abcdefgh',
+    'ab',
   ];
 
   const max = Math.max(...faces.map((variants) => variants.length));
 
   return (
     <div className="App">
+      <div className="mousefollowing">
+        <MouseFollowingDoomFace level={Math.ceil(Math.random() * 5) as LevelNumber} />
+      </div>
+
       <table>
         <tbody>
+          <tr>
+            <td />
+            <td>a</td>
+            <td>b</td>
+            <td>c</td>
+            <td>d</td>
+            <td>e</td>
+            <td>f</td>
+            <td>g</td>
+            <td>h</td>
+          </tr>
           {faces.map((variants, level) => (
+            // eslint-disable-next-line react/no-array-index-key
             <tr key={level}>
-              {variants.split("").map((variant: any) => {
+              <td>
+                {level + 1}
+              </td>
+              {variants.split('').map((variant: any) => {
                 const faceLevel: any = level + 1;
                 return (
                   <td key={variant}>
--- src/components/DoomFace/DoomFace.tsx
+++ src/components/DoomFace/DoomFace.tsx
@@ -1,25 +1,83 @@
-import React from "react"; // we need this to make JSX compile
-import classNames from "classnames";
+import React, { Ref } from 'react';
+import classNames from 'classnames';
 
-import classes from "./DoomFace.module.css";
+import classes from './DoomFace.module.css';
 
-export type levelNumber = 1 | 2 | 3 | 4 | 5 | 6;
-// export type levelNumber = number;
-export type variantLetter = "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h";
-// export type variantLetter = string;
+export type LevelNumber = 1 | 2 | 3 | 4 | 5 | 6;
+export type VariantLetter = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h';
 export type DoomFaceProps = {
-  level: levelNumber;
-  variant: variantLetter;
+  level: LevelNumber;
+  variant: VariantLetter;
+};
+export type ElementCenter = {
+  x: number;
+  y: number;
 };
 
-export const DoomFace = ({ level, variant }: DoomFaceProps) => (
-  <span className={classes.root}>
+export const DoomFace = React.forwardRef(({
+  level,
+  variant,
+}: DoomFaceProps, ref: Ref<HTMLElement>) => (
+  <span ref={ref} className={classes.root}>
     <span
       className={classNames(
         classes.sprite,
         classes[`level-${level}`],
-        classes[`variant-${variant}`]
+        classes[`variant-${variant}`],
       )}
     />
   </span>
-);
+));
+
+export type MouseFollowingDoomFaceProps = {
+  level: LevelNumber;
+};
+
+export function getElementCenter(el: HTMLElement): ElementCenter {
+  const {
+    x,
+    y,
+    width,
+    height,
+  } = el.getBoundingClientRect();
+  return {
+    x: x + (width * 0.5),
+    y: y + (height * 0.5),
+  };
+}
+
+export const MouseFollowingDoomFace = ({ level }: MouseFollowingDoomFaceProps) => {
+  const ref = React.useRef<HTMLElement>(null);
+  const [variant, setVariant] = React.useState<VariantLetter>('a');
+
+  React.useEffect(() => {
+    const mouseMoveListener = (evt: MouseEvent) => setVariant(() => {
+      if (!ref.current) return 'a';
+
+      const faceCenter = getElementCenter(ref.current);
+      const lookingLeft = faceCenter.x > evt.clientX;
+      const lookingFarHorizontally = Math.abs(faceCenter.x - evt.clientX) > 100;
+
+      if (lookingLeft && lookingFarHorizontally) return 'g';
+      if (lookingLeft && !lookingFarHorizontally) return 'f';
+      if (!lookingLeft && lookingFarHorizontally) return 'h';
+      if (!lookingLeft && !lookingFarHorizontally) return 'd';
+
+      return 'a';
+    });
+
+    document.addEventListener('mousemove', mouseMoveListener);
+
+    return () => {
+      document.removeEventListener('mousemove', mouseMoveListener);
+    };
+  });
+
+  return (
+    <DoomFace
+      ref={ref}
+      level={level}
+      variant={variant}
+    />
+  );
+};
